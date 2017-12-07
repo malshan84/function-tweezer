35411f8a4a3ea43a97df8b7064e8baf2b1be9db6
malshan <malshan84@gmail.com>
2017-12-07 18:39:30 +0900

    add logcollect event

--- src/events/LogCollectEvents.ts
+++ src/events/LogCollectEvents.ts
@@ -4,39 +4,57 @@ import { ipcRenderer } from 'electron';
 import * as UserinfoApi from '../api/UserInfo';
 import { UserInfo, SvcKind } from '../api/UserInfo';
 
+let gitLogCollector: Log.LogCollector;
+
 interface IReqInfo {
     start: number;
     end: number;
     localPath: string;
 }
 
+export interface IRevisionInfo {
+    revnum: string;
+    author: string;
+    message: string;
+    date: string;
+    diff: string;
+}
+
+let infos: Log.RevisionInfo[];
+function getRevInfos(err: string | null, revs: Log.RevisionInfo[]) {
+    var promise = new Promise((resolve, reject) => {
+        if (err !== null) {
+            console.log('error!!');
+            reject('error'); // pass values
+        } else {
+            infos = revs;
+            console.log(infos);
+            resolve('done'); // pass values
+        }
+    });
+    return promise;
+}
+
 const SHOW_LOG = 'SHOW_LOG';
 function showLogEvent() {
     let userInfo: UserInfo = UserinfoApi.getUserInfo(SvcKind.SVN);
-    const gitLogCollector = new LogCollector({username: userInfo.id, password: userInfo.pw, kind: 'git'});
+    gitLogCollector = new LogCollector({username: userInfo.id, password: userInfo.pw, kind: 'git'});
     RouteServer.getInstance().addEvent(SHOW_LOG, (req): any => {
         console.log(req.body);
-        const reqInfo: IReqInfo = req.body;
-        let infos: Log.RevisionInfo[];
+        const reqInfo: IReqInfo = req.body; 
 
         gitLogCollector.getLogWithRange(
             reqInfo.localPath, 
             {startLine: reqInfo.start, endLine: reqInfo.end}, 
-            100, 
-            (err: string|null, revs: Log.RevisionInfo[]) => {
-                if ( err !== null ) {
-                    console.log('error!!');
-                } else {
-                    infos = revs;
-            }
-        });
-
-        return infos;
+            10,
+            getRevInfos
+        );
     });
+    return infos;
 }
 
-export function listenShowLog(eventFunc: (revisions: string[]) => void) { 
-    ipcRenderer.on(SHOW_LOG, (event: Electron.Event, revisions: any) => { 
+export function listenShowLog(eventFunc: (revisions: IRevisionInfo[]) => void) { 
+    ipcRenderer.on(SHOW_LOG, (event: Electron.Event, revisions: IRevisionInfo[]) => {
         eventFunc(revisions);
     }); 
 }
